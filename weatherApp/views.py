8b9901daa8062d0b3d7e3d93bd709758fc2a6b09
Masum Hasan <masum2617@gmail.com>
2021-09-08 00:37:02 +0600

    layout updated

--- weatherApp/views.py
+++ weatherApp/views.py
@@ -4,34 +4,39 @@ from .models import City
 from .forms import CityForm
 
 # Create your views here.
+
+
 def index(request):
     url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=imperial&appid=ae091eb47d31f3860ad775f8cf242c29'
-    
-    city = 'Dhaka'
+    weather_data = []
 
     if request.method == "POST":
         form = CityForm(request.POST)
-        form.save()
+        if form.is_valid():
+            new_city = form.cleaned_data['name']
+
+            city_exists = City.objects.filter(name=new_city).exists()
+
+            if city_exists:
+                city = City.objects.get(name=new_city)
+
+                response = requests.get(url.format(city)).json()
+
+                city_weather = {
+                    'city': city.name,
+                    'temperature': response['main']['temp'] ,
+                    'description': response['weather'][0]['description'],
+                    'icon': response['weather'][0]['icon'],
+                }
+                weather_data.append(city_weather)
+                # print(city_weather)
+            else:
+                form.save()
 
 
     form = CityForm() # everytime form will restarted 
 
-    cities = City.objects.all()
-
-    weather_data = []
-
-    for city in cities:
-
-        response = requests.get(url.format(city)).json()
-
-        city_weather = {
-            'city': city.name,
-            'temperature': response['main']['temp'] ,
-            'description': response['weather'][0]['description'],
-            'icon': response['weather'][0]['icon'],
-        }
-        weather_data.append(city_weather)
-    # print(city_weather)
+    # cities = City.objects.all()
 
     context = {
         'weather_data': weather_data,
